c9f1e5779ac4cb7180114efcd6d78a84f6b7230a
Mantas Norvaiša <mntnorv@gmail.com>
2013-12-01 13:38:40 +0200

    Added cities table

---
+++ app/database/migrations/2013_12_01_105435_create_cities_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCitiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cities', function($table) {
+			$table->increments('id');
+			$table->string('name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('cities');
+	}
+
+}
