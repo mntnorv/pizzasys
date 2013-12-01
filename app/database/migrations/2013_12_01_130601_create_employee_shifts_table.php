3b8b985b1db31bf40beb46f3b7b0b024fc582fac
Mantas Norvaiša <mntnorv@gmail.com>
2013-12-01 15:07:40 +0200

    Added 'create_employee_shifts_table' migration

---
+++ app/database/migrations/2013_12_01_130601_create_employee_shifts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeShiftsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('employee_shifts', function($table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->time('starts_at');
+			$table->time('ends_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('employee_shifts');
+	}
+
+}
