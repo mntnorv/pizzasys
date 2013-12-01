54388f8e9037c0e3e88ca1489cad91aefc4bd5f7
Mantas Norvaiša <mntnorv@gmail.com>
2013-12-01 14:57:59 +0200

    Added 'create_food_ingredients_table' migration

---
+++ app/database/migrations/2013_12_01_125438_create_food_ingredients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodIngredientsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('food_ingredients', function($table) {
+			$table->increments('id');
+			$table->integer('food_id')->unsigned();
+			$table->foreign('food_id')->references('id')->on('food');
+			$table->integer('ingredient_id')->unsigned();
+			$table->foreign('ingredient_id')->references('id')->on('ingredients');
+			$table->double('quantity');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('food_ingredients');
+	}
+
+}
