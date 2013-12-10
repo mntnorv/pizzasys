e1e5fcd25ce7c824d75672a951d742f3347cb997
Mantas Norvaiša <mntnorv@gmail.com>
2013-12-10 18:10:04 +0200

    Added "Add to cart" buttons

--- app/assets/javascripts/home.food.js
+++ app/assets/javascripts/home.food.js
@@ -1,9 +1,21 @@
 $(function() {
 
 	//Thanks Master but I don't need your food in my SandBox
-	if($("#home-food-list").length == 0){
+	var foodList = $("#home-food-list");
+	if(foodList.length == 0){
 		return false;
 	}
+
+	var addToCart = function() {
+		var url = "api/cart/food";
+		var foodId = $(this).attr('data-food');
+
+		$.post(url, {'food_id': foodId})
+			.done(function (data) {
+				console.log(data);
+			})
+		;
+	};
 
 	var updateFoodItems = function(foodList, foodType) {
 		var url = "api/get/food";
@@ -16,6 +28,7 @@ $(function() {
 				foodList.html(
 					JST['handlebars/food_list'](data)
 				);
+				$('.food-item[data-food]').click(addToCart);
 			} else {
 				foodList.html('');
 				console.error('Error retrieving food: ' + data.error);
@@ -23,13 +36,10 @@ $(function() {
 		});
 	};
 
-	var foodList = $("#home-food-list");
-	if (foodList) {
-		window.onhashchange = function() {
-			var foodType = window.location.hash.slice(1);
-			updateFoodItems(foodList, foodType);
-		};
+	window.onhashchange = function() {
+		var foodType = window.location.hash.slice(1);
+		updateFoodItems(foodList, foodType);
+	};
 
-		window.onhashchange();
-	}
+	window.onhashchange();
 })
