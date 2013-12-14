5d8726fd090c3151e4a7096f77c260635e01f0e3
Justinas <justinas.maz@gmail.com>
2013-12-14 15:53:05 +0200

    Updated discounts view

--- app/views/admin/discounts.blade.php
+++ app/views/admin/discounts.blade.php
@@ -5,54 +5,57 @@
 <div class="container">
 	<div class="page-header">
 		<h1>Nuolaidos</h1>
+		<ol class="breadcrumb">
+			<li>Adminstravimas</li>
+			<li class="active">Nuolaidos</li>
+		</ol>
 	</div>
 
 	<!-- Grid-->
-	<div class="row">
-		<!-- Discounts manage form-->
-		<div class="col-sm-6">
-			<table id="discounts-table" class="table table-striped cart-table">
-				<colgroup>
-				<col class="name-column" />
-				<col class="type-column" />
-				<col class="percentage-column" />
-				<col class="actions-column" />
-			</colgroup>
-			<thead>
-				<tr>
-					<th>Pavadinimas</th>
-					<th>Tipas</th>
-					<th>Reikšmė (%)</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($discounts as $discount)
-				<tr class="cart-item">
-					<td>{{ $discount->discount->name }}</td>
-					<td>{{ $discount->discount->type }}</td>
-					<td>{{ $discount->discount->percentage }}</td>
-					<td>
-						<button class="btn btn-xs btn-danger remove-button fa fa-times"></button>
-					</td>
-				</tr>
-				@endforeach
+	<!-- Discounts display-->
+	<table id="discounts-table" class="table table-striped discounts-table">
+		<colgroup>
+		<col class="name-column" />
+		<col class="type-column" />
+		<col class="percentage-column" />
+		<col class="actions-column" />
+	</colgroup>
+	<thead>
+		<tr>
+			<th>Pavadinimas</th>
+			<th>Tipas</th>
+			<th>Reikšmė (%)</th>
+			<th></th>
+		</tr>
+	</thead>
+		<tfoot>
+		<tr>
+			<td></td>
+		</tr>
+	</tfoot>
+	<tbody>
+		@foreach ($discounts as $discount)
+		<tr class="cart-item">
+			<td>{{ $discount->name }}</td>
+			<td>{{ $discount->type }}</td>
+			<td>{{ $discount->percentage }}</td>
+			<td>
+				<button type="button" class="btn btn-s btn-default edit-button discounts-edit">Redaguoti</button> 
+				<button type="button" class="btn btn-s btn-danger remove-button fa fa-times"></button>
+			</td>
+		</tr>
+		@endforeach
 
-				@if (count($discounts) === 0)
-				<tr>
-					<td>Nėra nuolaidų</td>
-					<td></td>
-					<td></td>
-					<td></td>
-				</tr>
-				@endif
-			</tbody>
-		</table>
-	</div>
-
-
-
-
-</div>
+		@if (count($discounts) === 0)
+		<tr>
+			<td>Nėra nuolaidų</td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		@endif
+	</tbody>
+</table>
 </div>
 
 
